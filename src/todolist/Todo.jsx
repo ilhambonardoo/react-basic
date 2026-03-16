76d2e18a96745bcdd4be1ec44c1e6090a522e3c8
ilhambonardoo <ilhambonardo123@gmail.com>
2026-03-17 02:14:48 +0700

    conditional --> ternary operator

--- src/todolist/Todo.jsx
+++ src/todolist/Todo.jsx
@@ -1,14 +1,8 @@
 const Todo = ({ text, isCompleted, isDeleted = false }) => {
   if (isDeleted) {
     return null;
-  } else if (isCompleted) {
-    return (
-      <li>
-        <del>{text}</del>
-      </li>
-    );
   } else {
-    return <li>{text}</li>;
+    return <li>{isCompleted ? <del>{text}</del> : text}</li>;
   }
 };
 
