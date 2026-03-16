5e464adce1c6a45c0e32114a88eaf4a6a78f1b68
ilhambonardoo <ilhambonardo123@gmail.com>
2026-03-16 21:00:43 +0700

    style --> meggunakan file css

--- src/hello-world/HelloWorld.jsx
+++ src/hello-world/HelloWorld.jsx
@@ -1,3 +1,5 @@
+import "./HelloWorld.css";
+
 const HelloWorld = () => {
   const props = {
     text: "Hello World spread text",
@@ -13,25 +15,15 @@ const HelloWorld = () => {
 export default HelloWorld;
 
 function HeaderHelloWorld({ text = "Ups lupa kasih teks" }) {
-  return (
-    <h1
-      style={{
-        color: "red",
-        backgroundColor: "black",
-      }}
-    >
-      {text.toUpperCase()}
-    </h1>
-  );
+  return <h1 className="title">{text.toUpperCase()}</h1>;
 }
 
 function ParagraphHelloWorld() {
   const text = "Selamat Belajar ReactJs";
-  const style = { color: "blue", backgroundColor: "bisque" };
 
   return (
     <div>
-      <p style={style}>{text.toUpperCase()}</p>
+      <p className="content">{text.toUpperCase()}</p>
     </div>
   );
 }
