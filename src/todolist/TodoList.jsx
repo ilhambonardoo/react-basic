2bbbf4f4871d6d0434c55c08f134a6a827e624de
ilhambonardoo <ilhambonardo123@gmail.com>
2026-03-17 02:39:53 +0700

    component key (id) --> menggunakan key yang agar bisa memdakan data yang unique dan tidak berantakan pada saat ingin mengubah data

--- src/todolist/TodoList.jsx
+++ src/todolist/TodoList.jsx
@@ -3,23 +3,37 @@ import Todo from "./Todo";
 export default function TodoList() {
   const data = [
     {
+      id: 0,
       text: "Learn HTML",
       isCompleted: true,
     },
     {
+      id: 1,
       text: "Learn CSS",
       isCompleted: true,
     },
     {
+      id: 2,
       text: "Learn JAVSCRIPT",
       isCompleted: true,
     },
     {
+      id: 3,
       text: "Learn React JS",
+      isCompleted: false,
+    },
+    {
+      id: 4,
+      text: "Learn React JS Router",
       isCompleted: false,
     },
   ];
 
-  const todos = data.map((todo) => <Todo {...todo} />);
-  return <ul>{todos}</ul>;
+  return (
+    <ul>
+      {data.map((todo) => (
+        <Todo key={todo.id} {...todo} />
+      ))}
+    </ul>
+  );
 }
