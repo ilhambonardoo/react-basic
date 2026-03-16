45e691d3ff595e712b41c1483cbd17d73325199a
ilhambonardoo <ilhambonardo123@gmail.com>
2026-03-16 19:54:29 +0700

    membuat halaman hello world

---
+++ src/hello-world/HelloWorld.jsx
@@ -0,0 +1,12 @@
+const HelloWorld = () => {
+  return (
+    <>
+      <div>
+        <h1>Hello World</h1>
+        <p>Selamat Belajar React Js </p>
+      </div>
+    </>
+  );
+};
+
+export default HelloWorld;
