dc905aaa33498499a279cecf618154dae7593112
ilhambonardoo <ilhambonardo123@gmail.com>
2026-03-17 02:16:55 +0700

    conditional --> and logical

--- src/todolist/Todo.jsx
+++ src/todolist/Todo.jsx
@@ -2,7 +2,12 @@ const Todo = ({ text, isCompleted, isDeleted = false }) => {
   if (isDeleted) {
     return null;
   } else {
-    return <li>{isCompleted ? <del>{text}</del> : text}</li>;
+    return (
+      <li>
+        {text}
+        {isCompleted && "✅️"}
+      </li>
+    );
   }
 };
 
