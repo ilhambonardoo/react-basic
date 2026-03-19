54f2e0a7d55df51d062d6fc598eb2cb1b3e1770e
ilhambonardoo <ilhambonardo123@gmail.com>
2026-03-19 07:09:42 +0700

    sharing state --> untuk state yang diapakai bersama-sama, untuk membuat ini harus memindahkan lokasi statenya ke component paretnnya, lalu dikirim mealalu props. Misal, pada form task sebelumnya, kita membuat 2 child component, component untuk task form, dan component untuk task listnya. Share state dari task ke task form dan task list.

--- src/task/Task.jsx
+++ src/task/Task.jsx
@@ -1,37 +1,14 @@
-import { useState } from "react";
 import { useImmer } from "use-immer";
+import TaskForm from "./TaskForm";
+import TaskList from "./TaskList";
 
 const Task = () => {
-  const [item, setItem] = useState("");
   const [items, setItmes] = useImmer([]);
 
-  function handleChange(e) {
-    setItem(e.target.value);
-  }
-
-  function handleClick(e) {
-    e.preventDefault();
-    setItmes((draft) => {
-      draft.push(item);
-    });
-    setItem("");
-  }
   return (
     <>
-      <div>
-        <h1>Create Task</h1>
-        <form>
-          <input type="text" value={item} onChange={handleChange} />
-          <button onClick={handleClick}>Add</button>
-        </form>
-
-        <h1>Task List</h1>
-        <ul>
-          {items.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-      </div>
+      <TaskForm setItems={setItmes} />
+      <TaskList items={items} />
     </>
   );
 };
---
+++ src/task/TaskForm.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const TaskForm = ({ setItems }) => {
+  const [item, setItem] = useState("");
+
+  function handleChange(e) {
+    setItem(e.target.value);
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    setItems((draft) => {
+      draft.push(item);
+    });
+    setItem("");
+  }
+  return (
+    <>
+      <div>
+        <h1>Create Task</h1>
+        <form>
+          <input type="text" value={item} onChange={handleChange} />
+          <button onClick={handleClick}>Add</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default TaskForm;
