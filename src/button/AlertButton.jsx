8190490a90c9866cf19d573efc30c1d3d8f9d1e4
ilhambonardoo <ilhambonardo123@gmail.com>
2026-03-23 05:48:18 +0700

    Ref --> ini berguna untuk menyimpan data tanpa merender ulang component. Jadi misal ada data yang ingin disimpan useRef tidak akan merender ulang data tersebut.

--- src/button/AlertButton.jsx
+++ src/button/AlertButton.jsx
@@ -1,7 +1,11 @@
+import { useRef } from "react";
+
 const AlertButton = ({ text, message }) => {
+  const counter = useRef(0);
+
   function handleClick(e) {
     console.info(e.target);
-    alert(message);
+    alert(`${message} now ${counter.current++}`);
   }
   return <button onClick={handleClick}>{text}</button>;
 };
