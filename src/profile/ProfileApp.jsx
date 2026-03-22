f7e1e946c48df9b919221457df5328f4e5ebbd31
ilhambonardoo <ilhambonardo123@gmail.com>
2026-03-23 03:42:18 +0700

    Contect dan state --> untuk mengubah context harus menggunakan provider. Context hanya bisa diakses oleh component dibawahnya, tidak bisa component diatasnya atau yang sejajar. Kita menggunakan bantuan state di parent component sebagai contoh file profileForm itu menerima props dari parent component yaitu profile App

--- src/profile/ProfileApp.jsx
+++ src/profile/ProfileApp.jsx
@@ -1,13 +1,18 @@
 import ProfileAddress from "./ProfileAddress";
 import { ProfileContext } from "./ProfileContext";
 import Profile from "./Profile";
+import { useState } from "react";
+import ProfileForm from "./ProfileForm";
 
 const ProfileApp = () => {
+  const [name, setName] = useState("Ilham");
   return (
     <>
-      <ProfileContext.Provider value="Ilham">
+      <ProfileContext.Provider value={name}>
         <h1>Profile App</h1>
+        <ProfileForm name={name} setName={setName} />
         <Profile />
+
         <ProfileAddress />
       </ProfileContext.Provider>
     </>
