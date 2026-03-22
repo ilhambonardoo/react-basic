e51f2905b44c226a50757d15c7e0429de20905a2
ilhambonardoo <ilhambonardo123@gmail.com>
2026-03-22 09:06:25 +0700

    Reducer --> hooks state yang lainnya selain menggunakan useState, reducer ini dapat mengkonsolidasikan semua logic untuk update state di sebuah funcion luar component

--- src/note/NoteApp.jsx
+++ src/note/NoteApp.jsx
@@ -1,6 +1,6 @@
-import { useImmer } from "use-immer";
 import NoteForm from "./NoteForm";
 import NoteList from "./NoteList";
+import { useReducer } from "react";
 
 let id = 0;
 const initialNotes = [
@@ -26,24 +26,49 @@ const initialNotes = [
   },
 ];
 
+function notesReducer(notes, action) {
+  switch (action.type) {
+    case "ADD_NOTE":
+      return [
+        ...notes,
+        {
+          id: id++,
+          text: action.text,
+          done: false,
+        },
+      ];
+    case "CHANGE_NOTE":
+      return notes.map((note) =>
+        note.id === action.id
+          ? { ...note, text: action.text, done: action.done }
+          : note,
+      );
+    case "DELETE_NOTE":
+      return notes.filter((note) => note.id !== action.id);
+    default:
+      return notes;
+  }
+}
+
 const NoteApp = () => {
-  const [notes, setNotes] = useImmer(initialNotes);
+  const [notes, dispatch] = useReducer(notesReducer, initialNotes);
 
   function handleAddNote(text) {
-    setNotes((draft) => {
-      draft.push({ id: id++, text: text, done: false });
+    dispatch({
+      type: "ADD_NOTE",
+      text: text,
     });
   }
   function handleChangeNote(note) {
-    setNotes((draft) => {
-      const index = draft.findIndex((item) => item.id === note.id);
-      draft[index] = note;
+    dispatch({
+      type: "CHANGE_NOTE",
+      ...note,
     });
   }
   function handleDeleteNote(note) {
-    setNotes((draft) => {
-      const index = draft.findIndex((item) => item.id === note.id);
-      draft.splice(index, 1);
+    dispatch({
+      type: "DELETE_NOTE",
+      id: note.id,
     });
   }
 
