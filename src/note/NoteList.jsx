c63678980b0382c199c72113ea0ae300bf19b8e7
ilhambonardoo <ilhambonardo123@gmail.com>
2026-04-20 12:08:14 +0700

    useMemo --> useMemo berguna untuk mempercepat program komputer, dengan cara menyimpan data secara sementara dari hasil kalkulasi (yang berat), sehingga tidak perlu dikalkulasi ulang. Ini cocok untuk kasusu misal kita perlu memanggil kode yang berat, dan hasilnya selalu sama, dibandingkan kita panggil terus-terusan setiap proses render, lebih baik kita lakukan sekali saja, dan proses pemanggilan berikutnya, kita cukup mengembalikan hasil yang pertama.

--- src/note/NoteList.jsx
+++ src/note/NoteList.jsx
@@ -1,18 +1,35 @@
-import { useContext } from "react";
+import { useContext, useMemo, useRef, useState } from "react";
 import Note from "./Note";
-import { DispatchNotesContext, NotesContext } from "./NoteContext";
+import { NotesContext } from "./NoteContext";
 
 const NoteList = () => {
   const notes = useContext(NotesContext);
+  const [search, setSearch] = useState("");
+  const searchInput = useRef(null);
+
+  const filteredNotes = useMemo(() => {
+    console.info("Filtering Note");
+    return notes.filter((note) => note.text.includes(search));
+  }, [notes, search]);
+
+  function handleSearch() {
+    console.info("Searching.....");
+    setSearch(searchInput.current.value);
+  }
+
   return (
     <>
-      <ul>
-        {notes.map((note) => (
-          <li key={note.id}>
-            <Note note={note} />
-          </li>
-        ))}
-      </ul>
+      <div>
+        <input placeholder="Search" ref={searchInput} />
+        <button onClick={handleSearch}>Search</button>
+        <ul>
+          {filteredNotes.map((note) => (
+            <li key={note.id}>
+              <Note note={note} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
